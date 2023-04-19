62fd22e2c6d41456dab35712ca2c6610627551c0
Leon Gell <lgell@lcs.on.ca>
2023-04-19 19:07:18 -0400

    Added a search query for it to enter it into the database

--- FavouriteSnacks/Views/FavouriteView.swift
+++ FavouriteSnacks/Views/FavouriteView.swift
@@ -5,12 +5,20 @@
 //  Created by Leon Gell on 2023-04-19.
 //
 
+import Blackbird
 import SwiftUI
 
 struct FavouriteView: View {
     //MARK: Stored Properties
     
+    @Environment(\.blackbirdDatabase) var db:
+        Blackbird.Database?
     
+    @State var newItem: String = ""
+    
+    @State var newPrice: String = ""
+    
+    @State var newType: String = ""
     
     //MARK: Computed Properties
     var body: some View {
@@ -18,7 +26,34 @@ struct FavouriteView: View {
             
             VStack {
                 
-                
+                HStack {
+                    
+                    TextField("Snack", text: $newItem)
+                        .frame(maxWidth: 60)
+                    TextField("Price ?", text: $newPrice)
+                        .frame(maxWidth: 60)
+                    TextField("Sweet or Salty ?", text: $newType)
+                    
+                    Button(action: {
+                        
+                        Task {
+                            try await db!.transaction { core in
+                                try core.query("INSERT INTO FavouriteSnacks (item, price, type) VALUES (?,?,?)", newItem, newPrice, newType)
+                            }
+                            newItem = ""
+                            newPrice = ""
+                            newType = ""
+                        }
+                    }, label: {
+                        Text("ADD")
+                    })
+                    
+                    
+                    
+                    
+                    
+                }
+                .padding(10)
             }
             .navigationTitle("Favourite Snacks")
         }
