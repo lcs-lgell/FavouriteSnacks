640ab729f93e79b751fccaa875038e5f70b03f5c
Leon Gell <lgell@lcs.on.ca>
2023-04-19 10:31:41 -0400

    Added Blackbird to the app

--- FavouriteSnacks/FavouriteSnacksApp.swift
+++ FavouriteSnacks/FavouriteSnacksApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FavouriteSnacksApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FavouriteView()
         }
     }
 }
--- FavouriteSnacks/Views/FavouriteView.swift
+++ FavouriteSnacks/Views/FavouriteView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct FavouriteView: View {
+    //MARK: Stored Properties
+    
+    
+    
+    //MARK: Computed Properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
     }
 }
 
