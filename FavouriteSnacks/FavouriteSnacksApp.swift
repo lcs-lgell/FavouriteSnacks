736de5b4fac81fbc351b3562b4f0a4a874950536
Leon Gell <lgell@lcs.on.ca>
2023-04-19 10:16:22 -0400

    Initial Commit

---
+++ FavouriteSnacks/FavouriteSnacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteSnacksApp.swift
+//  FavouriteSnacks
+//
+//  Created by Leon Gell on 2023-04-19.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteSnacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
