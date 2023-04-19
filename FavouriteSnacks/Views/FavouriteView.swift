888521abaa078a01501420d31947285b2319df4e
Leon Gell <lgell@lcs.on.ca>
2023-04-19 10:23:22 -0400

    Re-organized the views in X-code.

---
+++ FavouriteSnacks/Views/FavouriteView.swift
@@ -0,0 +1,20 @@
+//
+//  FavouriteView.swift
+//  FavouriteSnacks
+//
+//  Created by Leon Gell on 2023-04-19.
+//
+
+import SwiftUI
+
+struct FavouriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavouriteView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavouriteView()
+    }
+}
